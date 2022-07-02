90a90203a684de5ebfa7bb90e4efc7976bd0ed57
raghadalju96 <raghad.alju96@gmail.com>
2022-07-02 15:00:37 +0300

    form validation

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  @Input() control : FormControl = new FormControl()
+  @Input() type :string = 'text'
+  @Input() placeholder:string = ''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,11 +8,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+    name: new FormControl('', [Validators.required,
+    Validators.minLength(3)]),
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('',[
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'/gm),
+    ]),
+    confirmPassword: new FormControl('',[
+      Validators.required
+    ]),
     phoneNumber: new FormControl(''),
   });
 
