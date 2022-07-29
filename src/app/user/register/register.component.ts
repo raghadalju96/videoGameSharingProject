36f0899d72c9325e33cf346fc6e84e79ca56b78c
rgaljuaid <rgaljuaid@saudia.com>
2022-07-30 00:58:03 +0300

    create a custom validate

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -5,6 +5,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
+
 
 @Component({
   selector: 'app-register',
@@ -19,7 +22,7 @@ export class RegisterComponent {
 
   registerForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email],[this.emailTaken.validate]),
     age: new FormControl<number |null>(null, [
       Validators.required,
       Validators.min(18),
@@ -37,9 +40,9 @@ export class RegisterComponent {
       Validators.minLength(13),
       Validators.maxLength(13),
     ]),
-  });
+  },[RegisterValidators.match('password','confirmPassword')]);
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private emailTaken:EmailTaken) {
     console.log(this.registerForm.controls.age.errors?.min);
   }
 
