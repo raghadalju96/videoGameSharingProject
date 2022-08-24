aecebade486ab2ac03fd9605cd470bc797eb1947
raghadalju96 <raghad.alju96@gmail.com>
2022-08-24 08:31:53 +0300

    uploade video

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,36 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+  showAlert: boolean = false;
+  alertMsg: string = 'Please wait! Your clip is being uploaded.';
+  alertColor: string = 'blue';
+  inSubmission: boolean = false;
+  persentage = 0
 
-  isDragover = false
-  file : File | null = null
-  nextStep = false
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
 
-  constructor() { }
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
 
-  ngOnInit(): void {
-  }
+  constructor(private storage: AngularFireStorage) {}
 
-  storeFile(event:Event){
-    this.isDragover = true
-    
-    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null 
+  ngOnInit(): void {}
+
+  storeFile(event: Event) {
+    this.isDragover = true;
+
+    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
     console.log(this.file);
     console.log(event);
 
-    if(!this.file || this.file.type !== 'video/mp4'){
-      return
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
     }
 
-   this.nextStep = true
-    
-    
-    
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
   }
 
+  uploadFile() {
+
+    this.showAlert = true;
+    this.alertMsg = 'Your clip is uploaded successfully';
+    this.alertColor = 'blue';
+    this.inSubmission = true; 
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+    const task = this.storage.upload(clipPath, this.file);
+    task.percentageChanges().subscribe(progress => {
+      this.persentage = progress as number / 100
+    })
+  }
 }
