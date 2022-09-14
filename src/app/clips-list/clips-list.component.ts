fdb5647bd71517c39b1e07dac407db916534bd7f
raghadalju96 <raghad.alju96@gmail.com>
2022-09-14 12:09:22 +0300

    Initializing the Video Player

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ClipService } from '../services/clip.service';
+
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
+  providers:[DatePipe]
 })
 export class ClipsListComponent implements OnInit, OnDestroy {
+
+  @Input() scrollable = true
 
   constructor(public clipService:ClipService) {
     clipService.getClips()
    }
 
   ngOnInit(): void {
+    if(this.scrollable){
 
     window.addEventListener('scroll', this.handelScroll)
-  }
+  }}
 
   ngOnDestroy(): void {
+    if(this.scrollable){
      window.removeEventListener('scroll', this.handelScroll)
   }
+  this.clipService.pageClips = []
+}
 
   handelScroll = () => {
     const {scrollTop, offsetHeight} = document.documentElement
