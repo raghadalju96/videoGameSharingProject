ad6af377b20be06ce365abda758c0d72e9274105
raghadalju96 <raghad.alju96@gmail.com>
2022-09-27 09:18:22 +0300

    testing nested component

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 import { NavComponent } from './nav.component';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthService', ['createUser', 'logout'], {isAuthenticated$: of(true)}
+  )
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      providers:[{provide:AuthService, useValue:mockedAuthService}]
     })
     .compileComponents();
 
